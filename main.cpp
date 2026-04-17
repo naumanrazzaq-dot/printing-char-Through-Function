4801421536a885e0e6eaf88d58c98e3b17565f45
Muhammad Nauman <maliknomigep@gmail.com>
2026-04-17 17:34:42 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  char ch='a';
+
+    for(int i=0; i<=3; i++){
+
+        for(int j=0; j<=6; j++){
+            cout<<ch<<" ";
+            ch++;
+            }
+       cout<<endl;
+    }
+    return 0;
+}
